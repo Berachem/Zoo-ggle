fab4f55aad481b98e19c4794fd842ba6ebff9dbe
Kilouhardi <Joshual.courriel@gmail.com>
2022-10-14 09:41:25 +0200

    fonction exportStaticTree
    voila la fonction export Static Tree du cours C qu'on aura besoin pour le projet.

---
+++ exportStaticTree.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+#define NONE -1
+
+//définitions des structures
+typedef int Element;
+
+typedef struct node{
+    Element elem;
+    struct node* firstChild;
+    struct node* nextSibling;
+} Node;
+typedef Node* CSTree;
+
+
+typedef struct {
+    Element elem;
+    unsigned int firstChild;
+    unsigned int nSiblings;
+} ArrayCell;
+typedef struct {
+    ArrayCell* nodeArray;
+    unsigned int nNodes;
+} StaticTree;
+
+
+//fonctions nécéssaires à l'export
+ArrayCell cons(Element elem ,int firstChild, int nSiblings){
+    ArrayCell l;
+    l.elem = elem;
+    l.firstChild = firstChild;
+    l.nSiblings = nSiblings;
+    return l;
+}
+
+int filltab(ArrayCell* tab,int size,int index,CSTree t){
+    if(t==NULL){return size;}
+
+    int enfant = nChildren(t);
+    int oldsize=size;
+    if(enfant==0){
+        tab[index]=cons(t->elem,NONE,nSibling(t)-1);
+    }else{
+        tab[index]=cons(t->elem,size,nSibling(t)-1);
+        int oldsize = size;
+        size+=enfant;
+    }
+    index+=1;
+    size = filltab(tab,size,index,t->nextSibling);
+    size = filltab(tab,size,oldsize,t->firstChild);
+
+    return size;    
+}
+
+StaticTree exportStaticTree(CSTree t){
+    int sizeOfTree = size(t);
+    StaticTree r;
+    r.nNodes = sizeOfTree;
+
+    ArrayCell* tab = malloc(sizeof(ArrayCell*)*sizeOfTree);
+    filltab(tab,nSibling(t),0,t);
+    r.nodeArray = tab;
+    return r;
+}
