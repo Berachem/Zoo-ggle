f33e56a29eda9fd92087001d7865f818c68966bf
Kilouhardi <Joshual.courriel@gmail.com>
2022-10-18 22:33:28 +0200

    squelette to .lex
    La fonction génère des chaines de caractère pour chaque entrée du staticTree.
    Le problème est que le fichier ne s'ouvre pas en écriture. (en tout cas ca ne dépasse pas le fopen)

--- game_motor/dictionnary_build.c
+++ game_motor/dictionnary_build.c
@@ -61,15 +61,51 @@ void printCSTree(CSTree t) {
   }
 }
 
+void convertStaticToLex(char *filename, StaticTree t){
 
-void main(int argc, char *argv[]) {
+    //c'est ici que ca plante (le fichier ne s'ouvre pas)
+    FILE *file = fopen(filename, "w");
+    
+    if (file == NULL) {
+        printf("Error with file %s", *filename);
+        exit(1);
+    }
+
+    //cette partie marche wallah j'ai testé
+    for(int i=0;i<t.nNodes;i++){
+        //j'initialise les 3 parties de ma chainse
+        char noeud[10];
+        char indexEnfant[10]; 
+        char nombreFrere[10];
+
+        //je transtype mes cases de int à string
+        sprintf(noeud,"%d",t.nodeArray[i].elem);  
+        sprintf(indexEnfant,"%d",t.nodeArray[i].firstChild);
+        sprintf(nombreFrere,"%d",t.nodeArray[i].nSiblings);           
+
+        //je crée ma string à l'aide de la concatenation (format X;X;X)
+        char ligne[50];
+        strcat(strcpy(ligne,noeud),";");
+        strcat(ligne,indexEnfant);
+        strcat(ligne,";");
+        strcat(ligne,nombreFrere);
+        printf("%s\n",ligne);
+
+        fwrite(ligne,sizeof(ligne),1,file);
+    }
+
+
+    fclose(file);
+}
+
+
+void main(int argc, char *argv[]) { 
   if (argc != 3) {
     printf("Erreur d'arguments");
     exit(1);
   }
-  CSTree t = convertFileToCSTree(&argv[1]); //"../data/dico.txt"
+  CSTree t = convertFileToCSTree(&argv[1]);   
   printCSTree(t);
   StaticTree st = exportStaticTree(t);
-  
-
+  //convertStaticToLex("../data/dico.lex",st); ne fonctionne pas totalement
 }
