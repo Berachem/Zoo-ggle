f016b5deffd6be07f97a42d45422142c7d05d3e3
Kilouhardi <Joshual.courriel@gmail.com>
2022-10-25 12:05:11 +0200

    création du java

---
+++ java/dico/src/main/java/Dictionnaire/dico/App.java
@@ -0,0 +1,13 @@
+package Dictionnaire.dico;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
