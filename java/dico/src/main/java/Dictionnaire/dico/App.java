55f8d33934ade92b8fda96a9c960f3cc1216a1ba
Kilouhardi <Joshual.courriel@gmail.com>
2022-11-06 17:47:50 +0100

    petites modifs commentaires

--- java/dico/src/main/java/Dictionnaire/dico/App.java
+++ java/dico/src/main/java/Dictionnaire/dico/App.java
@@ -11,8 +11,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
-
 public class App 
 {
 	
@@ -109,17 +107,12 @@ public class App
 		
 	}
 	
-	public static void resetFile(File file) {
-		if(file.exists()) {
-			file.delete();
-		}
-		try {
-			file.createNewFile();
-		}catch(Exception e) {
-			System.out.println("Error while creating file");
-		}
-	}
-	
+	/**
+	 * Prend un mot, en enleve les accents et le met en minuscule
+	 * 
+	 * @param mot : mot à nettoyer
+	 * @return le mot en minuscule sans ses accents
+	 */
 	public static String cleanupWord(String mot) {
 		
 		List<Character> accent  = List.of('À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ò', 'ó', 'ô', 'õ', 'ö', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ');
@@ -133,18 +126,30 @@ public class App
 		return mot;
 	}
 	
+	/**
+	 * Fonction qui vide un fichier, si il existe
+	 * 
+	 * @param file : le fichier a vider
+	 */
+	public static void resetFile(File file) {
+		if(file.exists()) {
+			file.delete();
+		}
+		try {
+			file.createNewFile();
+		}catch(Exception e) {
+			System.out.println("Error while creating file");
+		}
+	}
 	
-    public static void main( String[] args )
-    {
-        // ===================A CHANGER EN FONCTION DE L'ENDROIT OU VOUS AVEZ MIS LE FICHIER XML===================
-        String xmlPath = "C:\\Users\\Jlwis\\Desktop\\wiki-fr.xml"; // Path to the XML file
-
-		// JOSHUA : "C:\\Users\\Jlwis\\Desktop\\wiki-fr.xml"
-		// BERA : "C:\\Users\\berac\\Desktop\\wiki-fr.xml"
-        // ========================================================================================================
-        
-        //création  du dictionnaire pour les fréquences
-        HashMap<String,Integer> dicoFreq= new HashMap<>();
+	
+	/**
+	 * Fonction qui crée le dictionnaire des fréquences 
+	 * 
+	 * @return un HashMap<String,Integer> représentant le dictionnaire des fréquences
+	 */
+	public static HashMap<String,Integer> createDicoFreq(){
+		HashMap<String,Integer> dicoFreq= new HashMap<>();
         dicoFreq.put("a", 0);
         dicoFreq.put("b", 0);
         dicoFreq.put("c", 0);
@@ -172,6 +177,21 @@ public class App
         dicoFreq.put("y", 0);
         dicoFreq.put("z", 0);
         
+        return dicoFreq;
+	}
+	
+    public static void main( String[] args )
+    {
+        // ===================A CHANGER EN FONCTION DE L'ENDROIT OU VOUS AVEZ MIS LE FICHIER XML===================
+        String xmlPath = "C:\\Users\\Jlwis\\Desktop\\wiki-fr.xml"; // Path to the XML file
+
+		// JOSHUA : "C:\\Users\\Jlwis\\Desktop\\wiki-fr.xml"
+		// BERA : "C:\\Users\\berac\\Desktop\\wiki-fr.xml"
+        // ========================================================================================================
+        
+        //création  du dictionnaire pour les fréquences
+        HashMap<String,Integer> dicoFreq = App.createDicoFreq();
+        
 
 		// créer un fichier dico.json.txt en mode écriture
 		File dicoJSON = new File("dico.json.txt");
@@ -183,6 +203,8 @@ public class App
         
         try{
 	        
+        	
+        	// ============= CREATION DES ENTREE SORTIES =============
         	//creation du buffer de lecture du xml
 	        File file=new File(xmlPath);
 	        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
@@ -193,14 +215,18 @@ public class App
 	        
 	        //creation du writer pour le txt
 	        FileWriter writerFreq = new FileWriter(dicoFrequence, Charset.forName("UTF-8"));
+	        // =======================================================
 	        
 	        
 			// =============CHOIX DE LA LANGUE CIBLEE=================
 			String langueCible = "fr"; // fr = francais, en = anglais, es = espagnol, de = allemand ...
 			// =======================================================
 
-			// on crée les varaibles qui nous serviront pour la suite
-	        String page = "";
+			
+			
+			// ============= INITIALISATION DES VARIABLES ============ 
+	        //creations des variables pour la lecture du fichier xml
+			String page = "";
 	        String nameSpace = "";
 	        String mot = "";
 	        Boolean estMot = false;
@@ -209,41 +235,35 @@ public class App
 	        ArrayList<String> definitionsNom = new ArrayList<>();
 	        ArrayList<String> definitionsVerbe = new ArrayList<>();
 
-			// on crée les json
+			// on crée variables pour l'écriture du json
 			String jsonEntete = "{" +"\"description\" : \"dictionnaire francais\","+ "\"created_on\" :\""+ System.currentTimeMillis() +"\","+"\"language\" :"+"\""+langueCible+"\"}";
 			String jsonMot = "";
 
-			
-	        
-			// écris l'entete du json avec UTF-8
+			// écriture de l'entete du fichier json en UTF-8
 			writer.write(jsonEntete+"\n");
-
-	        String line="";
+			// =======================================================
+			
+			
+			
+			// ===== LECTURE ET ECRITURE DES DIFFERENTS FICHIERS =====
+	       
+			String line="";
 	        while((line=reader.readLine())!= null){
+	        	
+	        	
+	        	//******** AJOUT DES MOTS AU DICTIONNAIRES ********
 	        	if (line.contains("</page>")) {
 	        		dansUnePage = false;
 	        		
-					
 	        		if (estMot && estBonneLangue) {
 
-						// on affiche le mot et ses définitions 
-	        			System.out.println("\n\nMOT : "+ mot +"\n");
-						System.out.println("nom : \n");
-						definitionsNom.stream().forEach(s -> System.out.println(s)) ;
-						System.out.println("verbe : \n");
-						definitionsVerbe.stream().forEach(s -> System.out.println(s)) ;
-
-						// on crée un json du mot
+						//Actualisation du fichier json
 						jsonMot = "{" + "\"title\" : " + mot + "," + "\"definitions\":{\"nom\" : " + definitionsNom + "," + "\"verbe\" : " + definitionsVerbe + "}}";
-
-						// append le mot dans le json avec UTF-8
 						writer.write(jsonMot+"\n");
 						
 						
-						
-						//gestion des fréquences
-						
-							//on vire les accents pour simplifier 
+						//Actualisation des fréquence de lettre
+						mot = App.cleanupWord(mot); 
 		        		
 		        		for(int i=0;i<mot.length();i++) {
 		        			char lettre = mot.charAt(i);
@@ -251,19 +271,16 @@ public class App
 		        				dicoFreq.merge(String.valueOf(lettre), 1, Integer::sum);
 		        			}
 		        		}
-		        		System.out.println(dicoFreq);
-						
-						
-						
+		        		
+		        		//différents prints de debugs
+	        			System.out.println("\n\nMOT : "+ mot +"\n");
+						System.out.println("nom : \n");
+						definitionsNom.stream().forEach(s -> System.out.println(s)) ;
+						System.out.println("verbe : \n");
+						definitionsVerbe.stream().forEach(s -> System.out.println(s)) ;
+		        		System.out.println(dicoFreq);		        		
 	        		}
-	        		
-	        		
-	        		
-	        		
-	        		
-	        		
-	        		
-	        		// remet à 0 les variable car on quitte une section <page>
+	        		// remise à 0 des variable car on quitte une section <page>
 	        		page ="";
 	        		nameSpace = "";
 	        		mot = "";
@@ -272,13 +289,21 @@ public class App
 	        		definitionsNom = new ArrayList<>();
 	        		definitionsVerbe = new ArrayList<>();
 	        		
-	        	
-	        		
+	        		//passage à la ligne suivante (optimisation)
 	        		continue;
 	        	}
+	        	
+	        	
+	        	//******** TRIAGE DES MOTS ********
+	        		//on note que chaque ligne du fichier ne contient qu'une information, 
+	        		//il n'est donc pas nécéssaire de faire tout les tests lorsque l'on trouve une des informations
+	        		//De la vienne les continue à chaque if.
+	        	
+	        	
+	        	
 	        	if (line.contains("<page>")) {
 	        		dansUnePage = true;
-	        		
+	        		continue;
 	        	}
 	        	page += line;
 	        	
@@ -289,7 +314,7 @@ public class App
 	        	}
 	        	
 	        	if(line.contains("<ns>")) {
-	        		//si ns = 0 c'est un mot sinon c'est autre chose dont on en veut pas
+	        		//si ns = 0 c'est un mot sinon c'est autre chose dont on ne veut pas
 	        		nameSpace = App.recupInterieurBalise(line);
 	        		if(nameSpace.equals("0")) {
 	        			estMot = true;
@@ -303,6 +328,7 @@ public class App
 	        		continue;
 	        	}
 	        	
+	        	//****** GESTION DES DEFINITIONS ******
 	        	if(estBonneLangue && estMot){
 					if (line.startsWith("# '")) {
 	        			definitionsVerbe.add(App.cleanupExemple('"'+line.replaceFirst("# ", "").replaceFirst("</text>", "")+'"'));
@@ -312,20 +338,25 @@ public class App
 	        		}
 	        		
 	        	}
-	        	
-	        	
-	        	
+	        		
 	        }
+	        // =======================================================
 	        
+	        
+	        
+	        //ecriture du fichier des fréquences 
 	        for(Map.Entry<String,Integer> entry : dicoFreq.entrySet()) {
 	        	writerFreq.write(entry.getKey()+" "+String.valueOf(entry.getValue())+"\n");
 	        } 
+	        
+	        
 	        //fermeture des différents buffers
 	        reader.close();
 	        writer.close();
 	        writerFreq.close();
 	    }
-        catch(Exception e){
+        
+        catch(Exception e){//catch car utilisation des bufferedReader/Writter
         	e.printStackTrace();
         }
         
