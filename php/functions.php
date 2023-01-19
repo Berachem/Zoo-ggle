5a05c7c8e599b4366e786086def6ea41139ffc9f
Berachem Markria <berachem.markria@gmail.com>
2023-01-19 12:36:16 +0100

    getNbPlayersInWaitingRoom for @Lucas

--- php/functions.php
+++ php/functions.php
@@ -189,6 +189,19 @@ function getPublicGames($langue, $mode, $nbJoueurs, $name) {
 }
 
 
+// Fonction qui renvoie le nombre de joueurs dans le salon d'attente d'une partie $idPartie
+// (on compte le nombre de Score à -1 dans la table B_Score)
+function getNbPlayersInWaitingRoom($idPartie) {
+    global $db;
+    $query = "SELECT COUNT(*) as nombre FROM B_Score WHERE IdPartie = ? AND Score = -1";
+    $params = [
+        [1, $idPartie, PDO::PARAM_INT]
+    ];
+    $result = $db->execQuery($query, $params);
+    return $result[0]->nombre;
+}
+
+
 function formatDateToSentence($date){
     $date = new DateTime($date);
     $date = $date->format('d/m/Y à H:i:s');
