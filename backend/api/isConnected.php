63ebe3931020fe9ea970ec9e95a1b9096d7858a2
Berachem Markria <berachem.markria@gmail.com>
2023-03-20 21:21:21 +0100

    isConnected API

---
+++ backend/api/isConnected.php
@@ -0,0 +1,25 @@
+<?php
+
+
+// Renvoie un JSON pour dire si l'utilisateur est connecté ou non sous la forme de JSON (success)
+// $_SESSION["user"]
+
+header('Content-Type: application/json');
+
+session_start();
+
+$response = array();
+
+if (!isset($_SESSION["user"])) {
+    // pas connecté (json)
+    $response["success"] = false;
+    $response["errorCode"] = 603; // user not connected
+    $response["redirect"] = '../index.php?notConnected=true';
+} else {
+    $response["success"] = true;
+}
+
+echo json_encode($response);
+
+?>
+
