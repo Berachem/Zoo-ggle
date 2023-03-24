8429f0c8186c4ae8fc38600f1204c43fc435fe09
Kilouhardi <Joshual.courriel@gmail.com>
2023-03-24 20:21:10 +0100

    petit jet acceuil

--- frontend/src/pages/accueil.tsx
+++ frontend/src/pages/accueil.tsx
@@ -1,14 +1,25 @@
 
+import { Button,Typography } from "@material-tailwind/react";
+import Logo from "../assets/images/Title.svg";
+
 export default function Jouer() {
+    
+    
+    
     return (
+        <div
+            className="bg-cover bg-center min-h-screen flex flex-col justify-center items-center"
+            style={{
+            backgroundImage: "url('https://img.freepik.com/vecteurs-libre/fond-jungle-detaille_23-2148949774.jpg?w=2000')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+        }}>
         <div className="flex flex-col items-center justify-center h-screen">
-        
-            
-            <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-                <h1 className="text-6xl font-bold">
-                 ACCUEIL A FAIRE
-                </h1>
+            <main className="flex flex-col items-center justify-center w-full flex-1  text-center">
+                <img src={Logo}/>
+                <Button variant="filled" color="white" className="m-2">Règles</Button>
             </main>
         </div>
+    </div>
     )
 };
