faebb686ed5e36f9b9a71c82d1d0a1922ac7af63
Kilouhardi <Joshual.courriel@gmail.com>
2023-03-27 21:58:50 +0200

    big GameGrid clicable

--- frontend/src/components/Zooggle/GameGrid.tsx
+++ frontend/src/components/Zooggle/GameGrid.tsx
@@ -1,17 +1,20 @@
 import "../../css/GameGrid.css";
 
+function switchColor(id : string){
+    document.getElementById(id)?.classList.toggle("selected")
+}
+
 export default function GameGrid(props: any) {
     var gridClass = "grid "+`${props.width}`
     var pClass = "letter "+`${props.width}`
 
     return(
         <div className={gridClass}>
-            {props.grid.split(" ").map(function(item: string){
-                return(
-                    <p className={pClass}>
-                        {item}
-                    </p>)
-                })}
+            {
+                props.grid.split(" ").map((letter : string, index : number) => {
+                    return <div className={pClass} id={index.toString()} onClick={props.width == "small" ? (event: React.MouseEvent<HTMLElement>) => {} : (event: React.MouseEvent<HTMLElement>) => {switchColor(index.toString())}}>{letter}</div>
+                })
+            }
         </div>
         )
 }
