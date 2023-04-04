e9f18a1015262b53de426a04dbb301a0eac89852
Kilouhardi <Joshual.courriel@gmail.com>
2023-04-04 20:24:08 +0200

    Création formulaire Partie

--- frontend/src/pages/jouer.tsx
+++ frontend/src/pages/jouer.tsx
@@ -3,62 +3,69 @@ import GameCard from "../components/Zooggle/GameCard";
 import GameCardInfo from "../components/Zooggle/GameCardInfo";
 import Title from "../components/Zooggle/Title";
 import { Input, Button} from "@material-tailwind/react";
+import GameForm from "../components/Zooggle/GameForm";
+
+function popForm(id : string){
+    document.getElementById(id)?.classList.toggle("displayForm")
+}
 
 export default function Jouer() {
     return (
-
-        <div style={{
-            width:"min-content",
-            margin:"auto",
-            paddingTop:"10vh",
-            display:"flex",
-            flexDirection:"column",
-            alignItems:"center",
-            }}>
-            
-            <ZooggleCard width="50vw">
-                <Button size="lg" color="orange">Creer une partie</Button>
-            </ZooggleCard>
-
+        <>
+            <GameForm id="GameForm"/>
             <div style={{
+                width:"min-content",
+                margin:"auto",
+                paddingTop:"10vh",
                 display:"flex",
-                flexDirection:"row",
-                flexWrap:"wrap",
-                width:"98vw",
-                justifyContent:"space-evenly"
-            }}>
-                <ZooggleCard>
-                    <Title variant="h4">Rechercher une partie</Title>
-                    <div className="relative  flex w-full max-w-[24rem]" style={{margin:"auto",backgroundColor:"#FFFFFF",borderRadius:"0.5rem"}}>
-                        <Input type="text" label="Recherche" color="gray"/>
-                        <Button size="sm" color="orange" className="!absolute right-1 top-1 rounded">
-                            Envoyer
-                        </Button>
-                    </div>
+                flexDirection:"column",
+                alignItems:"center",
+                }}>
+                
+                <ZooggleCard width="50vw">
+                    <Button size="lg" color="orange" onClick={(event: React.MouseEvent<HTMLElement>) => {popForm("GameForm")}}>Creer une partie</Button>
                 </ZooggleCard>
 
-                <ZooggleCard width="">
-                    <Title variant="h4">Partie Récentes</Title>
-                    <GameCard>
-                        <GameCardInfo title="Ceci est un vrai titre oui" lang="francais" maker="Beraaaa" players="oui - non - Beraaa - Lucas123 - JoOOOOO"/>
-                    </GameCard>
-                    <GameCard>
-                        <GameCardInfo title="Ceci est un vrai titre oui" lang="francais" maker="Beraaaa" players="oui - non - Beraaa - Lucas123 - JoOOOOO"/>
-                    </GameCard>
-                    <GameCard>
-                        <GameCardInfo title="Ceci est un vrai titre oui" lang="francais" maker="Beraaaa" players="oui - non - Beraaa - Lucas123 - JoOOOOO"/>
-                    </GameCard>
-                    <GameCard>
-                        <GameCardInfo title="Ceci est un vrai titre oui" lang="francais" maker="Beraaaa" players="oui - non - Beraaa - Lucas123 - JoOOOOO"/>
-                    </GameCard>
-                    <GameCard>
-                        <GameCardInfo title="Ceci est un vrai titre oui" lang="francais" maker="Beraaaa" players="oui - non - Beraaa - Lucas123 - JoOOOOO"/>
-                    </GameCard>
-                </ZooggleCard>
+                <div style={{
+                    display:"flex",
+                    flexDirection:"row",
+                    flexWrap:"wrap",
+                    width:"98vw",
+                    justifyContent:"space-evenly"
+                }}>
+                    <ZooggleCard>
+                        <Title variant="h4">Rechercher une partie</Title>
+                        <div className="relative  flex w-full max-w-[24rem]" style={{margin:"auto",backgroundColor:"#FFFFFF",borderRadius:"0.5rem"}}>
+                            <Input type="text" label="Recherche" color="gray"/>
+                            <Button size="sm" color="orange" className="!absolute right-1 top-1 rounded">
+                                Envoyer
+                            </Button>
+                        </div>
+                    </ZooggleCard>
+
+                    <ZooggleCard width="">
+                        <Title variant="h4">Partie Récentes</Title>
+                        <GameCard>
+                            <GameCardInfo title="Ceci est un vrai titre oui" lang="francais" maker="Beraaaa" players="oui - non - Beraaa - Lucas123 - JoOOOOO"/>
+                        </GameCard>
+                        <GameCard>
+                            <GameCardInfo title="Ceci est un vrai titre oui" lang="francais" maker="Beraaaa" players="oui - non - Beraaa - Lucas123 - JoOOOOO"/>
+                        </GameCard>
+                        <GameCard>
+                            <GameCardInfo title="Ceci est un vrai titre oui" lang="francais" maker="Beraaaa" players="oui - non - Beraaa - Lucas123 - JoOOOOO"/>
+                        </GameCard>
+                        <GameCard>
+                            <GameCardInfo title="Ceci est un vrai titre oui" lang="francais" maker="Beraaaa" players="oui - non - Beraaa - Lucas123 - JoOOOOO"/>
+                        </GameCard>
+                        <GameCard>
+                            <GameCardInfo title="Ceci est un vrai titre oui" lang="francais" maker="Beraaaa" players="oui - non - Beraaa - Lucas123 - JoOOOOO"/>
+                        </GameCard>
+                    </ZooggleCard>
+                </div>
             </div>
-
-
-        </div>
+            
+        </>
+        
        
     )
 
