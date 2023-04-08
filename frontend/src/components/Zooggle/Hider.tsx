c3c7ca27c8f1d13fe9c013dc1d451b0945568d97
Kilouhardi <Joshual.courriel@gmail.com>
2023-04-08 15:26:38 +0200

    formulaire PC fait

--- frontend/src/components/Zooggle/Hider.tsx
+++ frontend/src/components/Zooggle/Hider.tsx
@@ -1,17 +1,53 @@
 import "../../css/ConnexionInscription.css"
 
+const textes=[
+    {
+        titre:"S'inscrire",
+        message:"Bienvenue sur Zoo-ggle! <br/>Le Boggle orienté animaux",
+        info:"Sinon cliquez en dessous pour vous connecter",
+        button:"Connexion"
+    },
+    {
+        titre:"Se connecter",
+        message:"Bonjour! <br/>Prêt à faire quelques parties?",
+        info:"Sinon cliquez en dessous pour vous inscrire",
+        button:"Inscription"
+    }
+]
+
+let index = 0
+
 function switchSide(id : string){
-    document.getElementById(id)?.classList.toggle("otherSide")
+    document.getElementById(id)?.classList.toggle("goRight")
+    document.getElementById(id)?.classList.toggle("goLeft")
+
+    index= (index+1)%2
+    let titre = document.getElementById("title")
+    let message =  document.getElementById("message")
+    let info =  document.getElementById("info")
+    let button = document.getElementById("button")
+    if(titre !== null){ //Typescript oblige la verification même si c'est codé en dur juste en dessous
+        titre.innerHTML=textes[index].titre
+    }
+    if(message !== null){
+        message.innerHTML=textes[index].message
+    }
+    if(info !== null){
+        info.innerHTML=textes[index].info
+    }
+    if(button !== null){
+        button.innerHTML=textes[index].button
+    }
 }
 
 export default function Hider(){
 
     return(
-        <div className="hider" id="hider">
-            <span className="hiderTitle">S'inscrire</span>
-            <span className="hiderMessage">Bienvenue sur Zoo-ggle ! <br/>Le Boggle orienté animaux</span>
-            <span className="hiderInfo">Sinon cliquez en dessous pour vous connecter</span>
-            <button className="hiderSubmit" onClick={(event: React.MouseEvent<HTMLElement>) => {switchSide("hider")}}>Connexion</button>
+        <div className="hider goRight" id="hider">
+            <span className="hiderTitle" id="title">S'inscrire</span>
+            <span className="hiderMessage" id="message">Bienvenue sur Zoo-ggle ! <br/>Le Boggle orienté animaux</span>
+            <span className="hiderInfo" id="info">Sinon cliquez en dessous pour vous connecter</span>
+            <button className="hiderSubmit" id="button" onClick={(event: React.MouseEvent<HTMLElement>) => {switchSide("hider")}}>Connexion</button>
         </div>
     )
 
--- frontend/src/components/Zooggle/Inscription.tsx
+++ frontend/src/components/Zooggle/Inscription.tsx
@@ -4,21 +4,21 @@ export default function Inscription(){
 
     return(
         <form action="" method="POST" className="connecForm">
-            <span className="connecLabel">Pseudo</span><br />
-            <input type="text" placeholder="votre pseudo" className="connecInput"/><br />
-            <span className="connecLabel">Mot de passe</span><br />
-            <input type="text" placeholder="votre mot de passe" className="connecInput"/><br />
-            <span className="connecLabel">Votre Adresse Mail</span><br />
-            <input type="text" placeholder="votre mot de passe" className="connecInput"/><br />
+            <span className="connecLabel">Pseudo</span>
+            <input type="text" placeholder="votre pseudo" className="connecInput"/>
+            <span className="connecLabel">Mot de passe</span>
+            <input type="text" placeholder="votre mot de passe" className="connecInput"/>
+            <span className="connecLabel">Votre Adresse Mail</span>
+            <input type="text" placeholder="votre mot de passe" className="connecInput"/>
 
-            <span className="connecLabel">Le type de compte</span><br />
+            <span className="connecLabel">Le type de compte</span>
             <div className="radioContainer">
                 <div><label htmlFor="prive">Privé</label><input type="radio" name="privatisation" value="prive" id="prive" checked/></div>
-                <div><label htmlFor="public">Public</label><input type="radio" name="privatisation" value="public" id="public"/><br /></div>
+                <div><label htmlFor="public">Public</label><input type="radio" name="privatisation" value="public" id="public"/></div>
             </div>
 
-            <span className="connecLabel">Votre Description</span><br />
-            <input type="textarea" placeholder="votre mot de passe" className="connecInput"/><br />
+            <span className="connecLabel">Votre Description</span>
+            <input type="textarea" placeholder="votre mot de passe" className="connecInput"/>
             <input type="submit" className="connecSubmit" value="s'inscrire"/>
         </form>
     )
