aed50b28c10078d315ba43dc4d9dfdff63aabc15
Berachem Markria <berachem.markria@gmail.com>
2023-05-07 16:26:10 +0200

    UI dictionnary fix

--- frontend/src/pages/dictionnaire.tsx
+++ frontend/src/pages/dictionnaire.tsx
@@ -22,8 +22,12 @@ const API_URL_FR = "http://localhost:3000/backend/api/getDefinitionOfWord?word="
 function Dictionnaire() {
   const [language, setLanguage] = useState("en");
   const [searchTerm, setSearchTerm] = useState("");
-  const [definitions, setDefinition] = useState([]);
+  const [definitionsENG, setDefinition] = useState([]);
+  const [definitionDataFR, setDefinitionFR] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
+
+  
 
   const searchDefinitionEn = async () => {
       const response = await fetch(`${API_URL_ENG}/${searchTerm}`);
@@ -49,8 +53,9 @@ function Dictionnaire() {
   
   const searchDefinitionFr = async () => {
     const response = await fetch(`${API_URL_FR}${searchTerm}`);
-    const data = await response.json();
-    console.log(data);
+    const data = await response.json(); // string yaml format
+    console.log(data); 
+    setDefinitionFR(data);
   };
   
   useEffect(() => {
@@ -84,7 +89,8 @@ function Dictionnaire() {
     navigator.clipboard.writeText(currentUrl);
   };
 
-  const isDefinitionAvailable = definitions.length > 0;
+  const isDefinitionAvailableENG = definitionsENG.length > 0;
+  const isDefinitionAvailableFR = definitionDataFR !== "";
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -112,12 +118,18 @@ function Dictionnaire() {
         </h1>
         {/* Radios diplayed in line for "Anglais" et "Francais" */}
         <div className="flex flex-row">
+        <img
+  alt="United Kingdom"
+  src="http://purecatamphetamine.github.io/country-flag-icons/3x2/GB.svg"
+  width="48"
+  height="32"
+  />
+
           <Radio
             color="red"
             id="english"
             name="language"
             value="en"
-            label="Anglais"
             checked={language === "en"}
             onChange={(e) => setLanguage(e.target.value)}
             labelProps={{ className: "text-white" }}
@@ -128,12 +140,17 @@ function Dictionnaire() {
             id="french"
             name="language"
             value="fr"
-            label="Français"
             checked={language === "fr"}
             onChange={(e) => setLanguage(e.target.value)}
             labelProps={{ className: "text-white" }}
             style={{ marginRight: "3.5px" }}
           />
+                  <img
+  alt="France"
+  src="http://purecatamphetamine.github.io/country-flag-icons/3x2/FR.svg"
+  width="48"
+  height="32"
+  />
         </div>
 
         <Input
@@ -144,20 +161,68 @@ function Dictionnaire() {
         />
         {/* <button className="bg-white text-black font-bold py-2 px-4 rounded hover:bg-green-400 hover:text-white"
         onClick={handleSearch}>  Rechercher </button> */}
-        <p className="text-center text-sm text-white">
-          Les définitions sont fournies par{" "}
+       {/*  <p className="text-center text-sm text-white font-bold">
+          Les définitions 
+          <span className="text-red-500"> Anglaises </span>
+          
+           sont fournies par{" "}
           <a
             href="https://dictionaryapi.dev/"
             target="_blank"
             rel="noreferrer"
+            className="text-red-500 hover:text-blue-600"
+          >
+            dictionaryapi.dev
+          </a>
+          et les définitions 
+          <span className="text-blue-500"> Françaises </span>
+           sont fournies par le{" "}
+          <a
+            href="https://fr.wiktionary.org/wiki/Wiktionnaire:Page_d%E2%80%99accueil"
+            target="_blank"
+            rel="noreferrer"
             className="text-blue-500 hover:text-blue-600"
+          >
+            Wiktionnaire
+          </a> sur lequel nous avons effectué un algorithme de recherche de définitions.
+        </p> */}
+
+        {language === "en" && (
+          <p className="text-center text-sm text-white font-bold">
+          Les définitions 
+          <span className="text-red-500"> Anglaises </span>
+          
+           sont fournies par{" "}
+          <a
+            href="https://dictionaryapi.dev/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-red-500 hover:text-blue-600"
           >
             dictionaryapi.dev
           </a>
         </p>
+        )}
+
+        {language === "fr" && (
+          <p className="text-center text-sm text-white font-bold">
+          Les définitions
+          <span className="text-blue-500"> Françaises </span>
+            sont fournies par le{" "}
+          <a
+            href="https://fr.wiktionary.org/wiki/Wiktionnaire:Page_d%E2%80%99accueil"
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-500 hover:text-blue-600"
+          >
+            Wiktionnaire
+          </a> sur lequel nous avons effectué un algorithme de recherche de définitions.
+        </p>
+        )}
+              
       </div>
 
-      {!isDefinitionAvailable && searchTerm !== "" && !isLoading && (
+      {!isDefinitionAvailableENG && !isDefinitionAvailableFR && searchTerm !== "" && !isLoading && (
         <div className="mt-4 bg-white rounded-lg shadow-xl w-full max-w-2xl px-4 py-6 space-y-4 justify-items-center">
           <h1 className="text-2xl font-bold mb-2">
             <FontAwesomeIcon
@@ -176,7 +241,7 @@ function Dictionnaire() {
           <PulseLoader color="#00BFA6" />
         </div>
       )}
-      {isDefinitionAvailable && searchTerm !== "" && (
+      {isDefinitionAvailableENG && searchTerm !== "" && language === "en" && (
         <div className="mt-4">
           <div className="flex flex-col items-center">
             <div className="text-center">
@@ -192,7 +257,7 @@ function Dictionnaire() {
                 </h1>
               </span>
 
-              {definitions.map((definition: any, index: number) => (
+              {definitionsENG.map((definition: any, index: number) => (
                 <div
                   key={index}
                   className="mt-4 shadow-xl rounded-lg p-4"
@@ -240,8 +305,19 @@ function Dictionnaire() {
           </div>
         </div>
       )}
+
+      {isDefinitionAvailableFR && searchTerm !== "" && language === "fr" && (
+        <div className="mt-4">
+          <h1>{searchTerm}</h1>
+          <div className="mt-4 shadow-xl rounded-lg p-4">
+             <p className="mb-2 text-justify">
+              {definitionDataFR}
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Dictionnaire;
