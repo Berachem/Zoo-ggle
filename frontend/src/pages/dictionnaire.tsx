bf3ea19b823e54737bc248cb699c81544400d56c
Berachem Markria <berachem.markria@gmail.com>
2023-05-07 16:26:37 +0200

    ui fix

--- frontend/src/pages/dictionnaire.tsx
+++ frontend/src/pages/dictionnaire.tsx
@@ -15,9 +15,8 @@ import {
 import { Radio, Select } from "@material-tailwind/react";
 
 const API_URL_ENG = "https://api.dictionaryapi.dev/api/v2/entries/en";
-const API_URL_FR = "http://localhost:3000/backend/api/getDefinitionOfWord?word=";
-
-
+const API_URL_FR =
+  "http://localhost:3000/backend/api/getDefinitionOfWord?word=";
 
 function Dictionnaire() {
   const [language, setLanguage] = useState("en");
@@ -26,38 +25,35 @@ function Dictionnaire() {
   const [definitionDataFR, setDefinitionFR] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-
-  
-
   const searchDefinitionEn = async () => {
-      const response = await fetch(`${API_URL_ENG}/${searchTerm}`);
-      const data = await response.json();
-      console.log(data);
-      if (data.title === "No Definitions Found") {
-        setDefinition([]);
-      } else {
-        const newDefinitions = data[0].meanings.map((meaning : any) => {
-          return {
-            partOfSpeech: meaning.partOfSpeech,
-            definitions: meaning.definitions.map(
-              (definition : any) => definition.definition
-            ),
-            examples: meaning.definitions.map(
-              (definition : any) => definition.example
-            ),
-          };
-        });
-        setDefinition(newDefinitions);
-      }
+    const response = await fetch(`${API_URL_ENG}/${searchTerm}`);
+    const data = await response.json();
+    console.log(data);
+    if (data.title === "No Definitions Found") {
+      setDefinition([]);
+    } else {
+      const newDefinitions = data[0].meanings.map((meaning: any) => {
+        return {
+          partOfSpeech: meaning.partOfSpeech,
+          definitions: meaning.definitions.map(
+            (definition: any) => definition.definition
+          ),
+          examples: meaning.definitions.map(
+            (definition: any) => definition.example
+          ),
+        };
+      });
+      setDefinition(newDefinitions);
+    }
   };
-  
+
   const searchDefinitionFr = async () => {
     const response = await fetch(`${API_URL_FR}${searchTerm}`);
     const data = await response.json(); // string yaml format
-    console.log(data); 
+    console.log(data);
     setDefinitionFR(data);
   };
-  
+
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       if (searchTerm !== "") {
@@ -66,7 +62,7 @@ function Dictionnaire() {
     }, 200);
     return () => clearTimeout(timeoutId);
   }, [searchTerm, language]);
-  
+
   const searchDefinition = async () => {
     setIsLoading(true);
     switch (language) {
@@ -118,12 +114,12 @@ function Dictionnaire() {
         </h1>
         {/* Radios diplayed in line for "Anglais" et "Francais" */}
         <div className="flex flex-row">
-        <img
-  alt="United Kingdom"
-  src="http://purecatamphetamine.github.io/country-flag-icons/3x2/GB.svg"
-  width="48"
-  height="32"
-  />
+          <img
+            alt="United Kingdom"
+            src="http://purecatamphetamine.github.io/country-flag-icons/3x2/GB.svg"
+            width="48"
+            height="32"
+          />
 
           <Radio
             color="red"
@@ -145,12 +141,12 @@ function Dictionnaire() {
             labelProps={{ className: "text-white" }}
             style={{ marginRight: "3.5px" }}
           />
-                  <img
-  alt="France"
-  src="http://purecatamphetamine.github.io/country-flag-icons/3x2/FR.svg"
-  width="48"
-  height="32"
-  />
+          <img
+            alt="France"
+            src="http://purecatamphetamine.github.io/country-flag-icons/3x2/FR.svg"
+            width="48"
+            height="32"
+          />
         </div>
 
         <Input
@@ -159,82 +155,58 @@ function Dictionnaire() {
           placeholder={"try..."}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        {/* <button className="bg-white text-black font-bold py-2 px-4 rounded hover:bg-green-400 hover:text-white"
-        onClick={handleSearch}>  Rechercher </button> */}
-       {/*  <p className="text-center text-sm text-white font-bold">
-          Les définitions 
-          <span className="text-red-500"> Anglaises </span>
-          
-           sont fournies par{" "}
-          <a
-            href="https://dictionaryapi.dev/"
-            target="_blank"
-            rel="noreferrer"
-            className="text-red-500 hover:text-blue-600"
-          >
-            dictionaryapi.dev
-          </a>
-          et les définitions 
-          <span className="text-blue-500"> Françaises </span>
-           sont fournies par le{" "}
-          <a
-            href="https://fr.wiktionary.org/wiki/Wiktionnaire:Page_d%E2%80%99accueil"
-            target="_blank"
-            rel="noreferrer"
-            className="text-blue-500 hover:text-blue-600"
-          >
-            Wiktionnaire
-          </a> sur lequel nous avons effectué un algorithme de recherche de définitions.
-        </p> */}
 
         {language === "en" && (
           <p className="text-center text-sm text-white font-bold">
-          Les définitions 
-          <span className="text-red-500"> Anglaises </span>
-          
-           sont fournies par{" "}
-          <a
-            href="https://dictionaryapi.dev/"
-            target="_blank"
-            rel="noreferrer"
-            className="text-red-500 hover:text-blue-600"
-          >
-            dictionaryapi.dev
-          </a>
-        </p>
+            Les définitions
+            <span className="text-red-500"> Anglaises </span>
+            sont fournies par{" "}
+            <a
+              href="https://dictionaryapi.dev/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-red-500 hover:text-blue-600"
+            >
+              dictionaryapi.dev
+            </a>
+          </p>
         )}
 
         {language === "fr" && (
           <p className="text-center text-sm text-white font-bold">
-          Les définitions
-          <span className="text-blue-500"> Françaises </span>
+            Les définitions
+            <span className="text-blue-500"> Françaises </span>
             sont fournies par le{" "}
-          <a
-            href="https://fr.wiktionary.org/wiki/Wiktionnaire:Page_d%E2%80%99accueil"
-            target="_blank"
-            rel="noreferrer"
-            className="text-blue-500 hover:text-blue-600"
-          >
-            Wiktionnaire
-          </a> sur lequel nous avons effectué un algorithme de recherche de définitions.
-        </p>
+            <a
+              href="https://fr.wiktionary.org/wiki/Wiktionnaire:Page_d%E2%80%99accueil"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-500 hover:text-blue-600"
+            >
+              Wiktionnaire
+            </a>{" "}
+            sur lequel nous avons effectué un algorithme de recherche de
+            définitions.
+          </p>
         )}
-              
       </div>
 
-      {!isDefinitionAvailableENG && !isDefinitionAvailableFR && searchTerm !== "" && !isLoading && (
-        <div className="mt-4 bg-white rounded-lg shadow-xl w-full max-w-2xl px-4 py-6 space-y-4 justify-items-center">
-          <h1 className="text-2xl font-bold mb-2">
-            <FontAwesomeIcon
-              icon={faExclamationCircle}
-              className="mr-2"
-              color="orange"
-              style={{ marginRight: "0.5rem" }}
-            />
-            Aucune définition trouvée
-          </h1>
-        </div>
-      )}
+      {!isDefinitionAvailableENG &&
+        !isDefinitionAvailableFR &&
+        searchTerm !== "" &&
+        !isLoading && (
+          <div className="mt-4 bg-white rounded-lg shadow-xl w-full max-w-2xl px-4 py-6 space-y-4 justify-items-center">
+            <h1 className="text-2xl font-bold mb-2">
+              <FontAwesomeIcon
+                icon={faExclamationCircle}
+                className="mr-2"
+                color="orange"
+                style={{ marginRight: "0.5rem" }}
+              />
+              Aucune définition trouvée
+            </h1>
+          </div>
+        )}
 
       {isLoading && (
         <div className="mt-4">
@@ -310,14 +282,12 @@ function Dictionnaire() {
         <div className="mt-4">
           <h1>{searchTerm}</h1>
           <div className="mt-4 shadow-xl rounded-lg p-4">
-             <p className="mb-2 text-justify">
-              {definitionDataFR}
-            </p>
+            <p className="mb-2 text-justify">{definitionDataFR}</p>
           </div>
         </div>
       )}
     </div>
   );
-};
+}
 
 export default Dictionnaire;
