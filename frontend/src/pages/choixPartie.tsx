a1199361627d86a7045c845f276d1be91d1955d3
Kilouhardi <Joshual.courriel@gmail.com>
2023-05-22 13:41:34 +0200

    oups le lion 2

--- frontend/src/pages/choixPartie.tsx
+++ frontend/src/pages/choixPartie.tsx
@@ -43,7 +43,7 @@ export default function Jouer() {
       joueurs: 2,
       dictionnaire: "fr",
       grille: 4,
-      couleur: "#579A86",
+      couleur: "#F5593D",
       cartoonImage:
         "https://cdn.discordapp.com/attachments/890989295794552832/1108097595299090652/1f981.png",
       realImage:
